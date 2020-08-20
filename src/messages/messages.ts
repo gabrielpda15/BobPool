09c3b088c9fc64a7d1aef1b18d77d724469faeec
gabrielpda15 <gabriel.pda15@gmail.com>
2020-08-20 16:21:24 -0300

    Inclusão da mensagem de blocked

--- src/messages/messages.ts
+++ src/messages/messages.ts
@@ -1,5 +1,6 @@
 import config from './config.json';
 import readme from './readme.json';
 import rules from './rules.json';
+import blocked from './blocked.json';
 
-export default { config: config, readme: readme, rules: rules };
+export default { config: config, readme: readme, rules: rules, blocked: blocked };
