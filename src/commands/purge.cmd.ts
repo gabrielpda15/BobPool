5fd2b56e1b8b771e6655fb34b0f1e73dbbc3989c
gabrielpda15 <gabriel.pda15@gmail.com>
2020-10-06 22:16:46 -0300

    Otimização purge

--- src/commands/purge.cmd.ts
+++ src/commands/purge.cmd.ts
@@ -25,12 +25,43 @@ class Purge implements ICommand {
             return;
         }
 
+        if (message.mentions.members.size == 1) {
+            await message.delete();
+            const mentionedUser = message.mentions.members.first();           
+
+            if (message.author.id === mentionedUser.id) n = n + 1;
+            
+            let tempMsg = await message.channel.send('Iniciando processo de exclusão...');
+
+            let i = n;
+            while (i > 0) {
+                let temp = await message.channel.messages.fetch({limit: 100});
+                temp = temp.filter(m => m.author.id == mentionedUser.id);
+                let count = 0;
+                temp = temp.reduce((rv, e) => {
+                    if (count++ < i) {
+                        return rv.set(e.id, e);
+                    }
+                    return rv;
+                }, new Discord.Collection<string, Discord.Message>());
+                await (message.channel as Discord.TextChannel).bulkDelete(temp, false);
+                i = i - temp.size;
+            }
+
+            await tempMsg.edit(':white_check_mark: Pronto!!!');
+            setTimeout(async () => await tempMsg.delete(), 3000);
+            return;
+        } else if (message.mentions.members.size > 1) {
+            await message.channel.send(':x: Mencione apenas um membro!');
+            return;
+        }
+
         n = n + 2;
 
         await message.channel.send('Iniciando processo de exclusão...');
 
         for (let i = n; i > 0; i = i - 100) {
-            if (n > 100) {
+            if (i > 100) {
                 await (message.channel as Discord.TextChannel).bulkDelete(100);
             } else {
                 await (message.channel as Discord.TextChannel).bulkDelete(i);
